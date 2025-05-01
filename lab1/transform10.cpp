1276a1fdedda07266cba76386a29ea9db07de230
CharmLynx <maryna.maliutina@gmail.com>
2025-05-01 09:37:42 +0300

    add upd

--- lab1/transform10.cpp
+++ lab1/transform10.cpp
@@ -38,11 +38,30 @@ void hex_32(const string& hex_string, uint64_t digits[], int& count){
     }
 }
 
+void long_add(uint64_t A[], uint64_t B[], uint64_t C[], int& carry){
+    carry=0;   
+    uint64_t temp; 
+    
+    for(int i=0;i<=32-1;i++){
+        temp=A[i]+B[i]+carry;
+        //cout<<"A[i] "<<A[i]<<endl;
+        //cout<<"B[i] "<<B[i]<<endl;
+        //cout<<"після додавання "<<temp<<endl;
+        C[i]=temp&(4294967296-1);
+        C[i] = temp & 0xFFFFFFFF;
+        //cout<<"c[i] "<<C[i]<<endl;
+        carry=temp >> 32;
+        //cout<<"перенос "<<carry<<endl;
+        //cout<<"-----------------"<<endl;
+        
+    }
+}
+
 
 int main(){
     
     string b = "12345ABCDEF9012345ABCDEF9012345ABCDEF9012345ABCDEF9012345ABCDEF9012345ABCDEF9012345ABCDEF9012345ABCDEF9012345ABCDEF9012345ABCDEF9012345ABCDEF9012345ABCDEF9012345ABCDEF9012345ABCDEF9012345ABCDEF9012345ABCDEF9012345ABCDEF9012345ABCDEF9012345ABCDEF90123452222";
-    string a = "283527121300cddeaa5d2750e2fabe17bc2289f575609de72dbd34d03ad2be472abec4f8cdb6653a8459867f72ff4840e9de7e9e3b8a08ce0427d24f14acf4f2ef1ace93e8b3ee9ec59f508c4e919a8a2e5cd550df1e31b387c67397f36423795907cc0c8a38f46c26979782030a9b5475db2902fac12161cc1ae853d68e00fe";   
+    string a = "ffffffff1300cddeaa5d2750e2fabe17bc2289f575609de72dbd34d03ad2be472abec4f8cdb6653a8459867f72ff4840e9de7e9e3b8a08ce0427d24f14acf4f2ef1ace93e8b3ee9ec59f508c4e919a8a2e5cd550df1e31b387c67397f36423795907cc0c8a38f46c26979782030a9b5475db2902fac12161cc1ae853d68e00fe";   
     //string a = "123456789abcdef9";
     //string b = "abcdef9012345678";
 
@@ -63,24 +82,15 @@ int main(){
         cout << hex << B[j] << endl;
     }*/
 
-    int carry=0;
-    uint64_t temp;
+    int carry;
     uint64_t C[64];
-    for(int i=0;i<=count_a-1;i++){
-        temp=A[i]+B[i]+carry;
-        //cout<<"після додавання "<<temp<<endl;
-        C[i]=temp&(4294967296-1);
-        //C[i] = temp & 0xFFFFFFFF;
-        //cout<<"c[i] "<<C[i]<<endl;
-        carry=temp >> 32;
-        //cout<<"перенос "<<carry<<endl;
-        //cout<<"-----------------"<<endl;
-        
-    }
+    long_add(A, B, C, carry);
 
-    cout<<endl;
+    //cout<<"carry "<<carry<<endl;
     //треба придумати як передбачати розмір масиву
-    for (int j = 31; j >=0; j--){
+    cout<<"add ";
+    if(carry!=0) cout<<carry;
+    for (int j = count_a-1; j >=0; j--){
         cout << hex << C[j]<<"";
     }
     
